82c573a6033b933d980ce0ca4bdaa1f67f8c8540
minghuadev <minghuadev@noreply.users.github.com>
2020-09-17 21:12:34 -0700

    picosoc-de0nano compile ok

---
+++ picosoc-de0nano-quartus/picosoc_de0nano.v
@@ -0,0 +1,28 @@
+/*
+ *  Raven - A full example SoC using PicoRV32 in X-Fab XH018
+ *
+ *  Copyright (C) 2017  Clifford Wolf <clifford@clifford.at>
+ *  Copyright (C) 2018  Tim Edwards <tim@efabless.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+`timescale 1 ns / 1 ps
+
+`include "../picosoc-de0nano/picosoc.v"
+`include "../picorv32.v"
+
+`include "../picosoc-de0nano/spimemio.v"
+`include "../picosoc-de0nano/simpleuart.v"
+
